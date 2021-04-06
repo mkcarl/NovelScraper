ebc3cba60f9bc001e72284244f02ecb15da64256
mkcarl <53103933+mkcarl@users.noreply.github.com>
2021-04-06 22:06:53 +0800

    done objective : scrape web novel and package into epub format

--- WebProcesses.py
+++ WebProcesses.py
@@ -1,14 +1,19 @@
 from bs4 import BeautifulSoup
-from requests_html import HTMLSession
+from requests_html import HTMLSession, AsyncHTMLSession
 import requests
-
+import asyncio
+import os
+import pathlib
 
 class Novel:
-    def __init__(self, url):
+    def __init__(self, url, firstChapter, lastChapter):
         self.base_url = url
+        self.url_chapter_formatter = 0
+        self.content = {}
         self._content_id = None
-        self._first_chapter = 1
-        self._last_chapter = None
+        self._first_chapter = firstChapter
+        self._last_chapter = lastChapter
+        self._name = "Default_Name"
 
     @property
     def content_id(self):
@@ -34,36 +39,73 @@ class Novel:
     def first_chapter(self, firstChapNum: int):
         self._first_chapter = firstChapNum
 
-    def extract_content(self):
-        if self._last_chapter == None:
-            raise Exception("Plese set the final chapter of the novel")
-        if self._content_id == None:
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, novel_name):
+        self._name = novel_name
+
+    def extract_content(self, start=None, end=None, asynchronous=False, ratelimit=None):
+        if self._last_chapter is None:
+            raise Exception("Please set the final chapter of the novel")
+        if self._content_id is None:
             raise Exception("Please define the ID of the content div.")
+        start_, end_ = start, end
+        if start is None:
+            start_ = self.first_chapter
+        if end is None:
+            end_ = self.last_chapter
 
-        contents = {}
-        for chapterNumber in range(self.first_chapter, self.last_chapter + 1):
-            chapter_url = self.base_url + str(chapterNumber)
-            session = HTMLSession()
-            webpage = session.get(chapter_url)
-            chapter_HTML = webpage.html.html
-            soup = BeautifulSoup(chapter_HTML)
-            chapter_content = soup.find(id=self.content_id).find_all('p')
-            contents[f"Chapter {str(chapterNumber)}"] = chapter_content
-        return contents
+        if not asynchronous:
+            for chapterNumber in range(start_, end_ + 1):
+                chapter_url = f"{self.base_url}{str(chapterNumber).zfill(self.url_chapter_formatter)}"
+                session = HTMLSession()
+                webpage = session.get(chapter_url)
+                chapter_HTML = webpage.html.html
+                soup = BeautifulSoup(chapter_HTML)
+                chapter_content = soup.find(id=self.content_id)
+                self.content[f"Chapter {str(chapterNumber)}"] = chapter_content
+                print(f"Done chapter {chapterNumber}")
 
-# first, determine page type (static or dynamic)
-# second, determine content id manually
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-# ;
+        elif asynchronous:
+            async def getChap(url):
+                aSession = AsyncHTMLSession()
+                r = await aSession.get(url)
+                return (url[len(self.base_url):],r.html.html)
+
+            urls = [f"{self.base_url}{str(x).zfill(self.url_chapter_formatter)}" for x in range(start_, end_ + 1)]
+            coros = [getChap(x) for x in urls]
+
+            async def getContents(coros):
+                contents = await asyncio.gather(*coros)
+                for i, content in contents:
+                    soup = BeautifulSoup(content)
+                    chapter_content = soup.find(id=self.content_id)
+                    self.content[f"Chapter_{str(i).zfill(len(str(self.last_chapter)))}"] = chapter_content
+
+            if ratelimit is None:
+                asyncio.run(getContents(coros))
+            elif ratelimit != 0:
+                chunks = [coros[i:i + ratelimit] for i in range(0, len(coros), ratelimit)]
+                for chunk in chunks:
+                    asyncio.run(getContents(chunk))
+                    print(f"Fetched chunk {chunks.index(chunk)+1}/{len(chunks)}")
+
+    def export(self, fmt):
+        directory = f"chapters/{self.name}"
+        if not os.path.exists(directory):
+            pathlib.Path(directory).mkdir(parents=True, exist_ok=True)
+        for chap, cont in self.content.items():
+            with open(f"{directory}/{chap}.{fmt}", "w", encoding="utf-8") as f:
+                f.write(str(cont))
+
+if __name__ == '__main__':
+    loa = Novel("https://www.wuxiaworld.com/novel/life-once-again/loa-chapter-",
+                0, 50)
+    loa._content_id = "chapter-content"
+    loa.name = "Life_Once_Again"
+    loa.extract_content(asynchronous=True, ratelimit=30)
+    loa.export("html")
+    print(len(loa.content))
--- main.py
+++ main.py
@@ -1,12 +1,48 @@
 import WebProcesses
+from ebooklib import epub
+import os
+import time
 
-NOVEL_BASE_URL = "https://www.wuxiaworld.com/novel/life-once-again/loa-chapter-"
+def main():
+    NOVEL_BASE_URL = "https://www.wuxiaworld.com/novel/rmji/rmji-chapter-"
+    novel = WebProcesses.Novel(NOVEL_BASE_URL, 0, 2110) # Change me
+    novel.content_id = "chapter-content" # Change me
+    novel.name = "A Record of a Mortal's Journey to Immortality".replace(' ', '_') # Change me
+    novel.extract_content(asynchronous=True, ratelimit=60)
+    novel.export("html")
 
-def main(base_url):
-    # do the actual pipeline of making it from web to epub
-    LOA = WebProcesses.Novel(base_url)
-    LOA.last_chapter = 10
-    LOA.content_id = "chapter-content"
-    print(LOA.extract_content().keys())
+    book = epub.EpubBook()
+    book.set_identifier("rmji") # Change me
+    book.set_title(novel.name.replace('_', ' '))
+    book.set_language("en")
+    book.add_author("Default Author") # Change me
 
-main(NOVEL_BASE_URL)
+    book_chapters = []
+    for chapter in os.listdir(f"chapters/{novel.name}"):
+        with open(f"chapters/{novel.name}/{chapter}", "r", encoding="utf-8") as f:
+            c = epub.EpubHtml(
+                title=chapter,
+                file_name=f"{chapter}.html"
+                )
+            c.set_content(f.read())
+            book_chapters.append(c)
+
+    for chapter in book_chapters:
+        book.add_item(chapter)
+    book_chapters_link = [epub.Link(x.file_name, x.title.split('.')[0], x.title.split('.')[0]) for x in book_chapters]
+    book.toc = tuple(book_chapters_link)
+    book.spine = book_chapters
+    book.add_item(epub.EpubNcx())
+    book.add_item(epub.EpubNav())
+
+    epub.write_epub(f"{book.title}.epub", book, {})
+
+def print_timer(func):
+    t1 = time.time()
+    func()
+    print(f"Process took {time.time() - t1} seconds")
+
+
+
+print_timer(main)
+
